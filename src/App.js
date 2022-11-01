ff6571273b9db5d964b2ad2c8f7eadffebca80ca
Elizabeth <tjeudy.e@gmail.com>
2022-11-01 15:16:39 -0400

    added github open source link

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
 import "./App.css";
 
 export default function App() {
-	return <div className="App">Weather App</div>;
+	return (
+		<div className="App">
+			Weather App
+			<footer>
+				<p>
+					<a
+						href="https://github.com/elizabethJeudy/weather-app-react"
+						target="_blank"
+					>
+						Open-Sourced
+					</a>{" "}
+					{""}
+					by: Elizabeth Jeudy
+				</p>
+			</footer>
+		</div>
+	);
 }
