fd0df24540b95e94fce97dbbd3688de35bb0dc37
Elizabeth <tjeudy.e@gmail.com>
2022-11-01 15:58:23 -0400

    imported bootstrap

--- src/App.js
+++ src/App.js
@@ -3,19 +3,21 @@ import "./App.css";
 export default function App() {
 	return (
 		<div className="App">
-			Weather App
-			<footer>
-				<p>
-					<a
-						href="https://github.com/elizabethJeudy/weather-app-react"
-						target="_blank"
-					>
-						Open-Sourced
-					</a>{" "}
-					{""}
-					by: Elizabeth Jeudy
-				</p>
-			</footer>
+			<div className="container">
+				Weather App
+				<footer>
+					<p>
+						<a
+							href="https://github.com/elizabethJeudy/weather-app-react"
+							target="_blank"
+						>
+							Open-Sourced
+						</a>{" "}
+						{""}
+						by: Elizabeth Jeudy
+					</p>
+				</footer>
+			</div>
 		</div>
 	);
 }
